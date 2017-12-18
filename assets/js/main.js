b73a8f61e01f8c5ae11d7209af8cca26dc5e8024
kazu <kobakazu10@gmail.com>
2017-12-18 23:04:37 +0900

    きれいにコードを書きなおした

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,65 +1,4 @@
 (function() {
-  var view = new ol.View({
-    center: ol.proj.fromLonLat([132.601271,34.232102]),
-    zoom: 16,
-    // minZoom: 15,//挙動がおかしいからやめたほうがいいかも
-    // maxZoom: 17
-    // そもそもいらないかな？
-  });
-
-  var geolocation = new ol.Geolocation({
-    projection: view.getProjection()
-  });
-
-  function el(id) {
-    return document.getElementById(id);
-  }
-
-  el('track').addEventListener('change', function() {
-    geolocation.setTracking(this.checked);
-  });
-
-  // update the HTML page when the position changes.
-  geolocation.on('change', function() {
-    el('accuracy').innerText = geolocation.getAccuracy() + ' [m]';
-    el('altitude').innerText = geolocation.getAltitude() + ' [m]';
-    el('altitudeAccuracy').innerText = geolocation.getAltitudeAccuracy() + ' [m]';
-    el('heading').innerText = geolocation.getHeading() + ' [rad]';
-    el('speed').innerText = geolocation.getSpeed() + ' [m/s]';
-  });
-
-  // handle geolocation error.
-  geolocation.on('error', function(error) {
-    var info = document.getElementById('info');
-    info.innerHTML = error.message;
-    info.style.display = '';
-  });
-
-  var accuracyFeature = new ol.Feature();
-  geolocation.on('change:accuracyGeometry', function() {
-    accuracyFeature.setGeometry(geolocation.getAccuracyGeometry());
-  });
-
-  var positionFeature = new ol.Feature();
-  positionFeature.setStyle(new ol.style.Style({
-    image: new ol.style.Circle({
-      radius: 6,
-      fill: new ol.style.Fill({
-        color: '#3399CC'
-      }),
-      stroke: new ol.style.Stroke({
-        color: '#fff',
-        width: 2
-      })
-    })
-  }));
-
-  geolocation.on('change:position', function() {
-    var coordinates = geolocation.getPosition();
-    positionFeature.setGeometry(coordinates ?
-      new ol.geom.Point(coordinates) : null);
-  });
-
   var map = new ol.Map({
     target: 'map',
     interactions: ol.interaction.defaults().extend([
@@ -71,74 +10,17 @@
     layers: [
       new ol.layer.Group({
         'title': 'Base maps',
-        layers: [
-          new ol.layer.Tile({
-            title: '国土地理院',
-            type: 'base',
-            visible: false,//trueならば最初から表示してくれる、ただし1つまで
-            source: new ol.source.XYZ({
-              attributions: [
-                new ol.Attribution({
-                  html: "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>"
-                })
-              ],
-              url: "https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png",
-              projection: "EPSG:3857"
-            })
-          }),
-      new ol.layer.Group({
-        title: 'Water color with labels',
-        type: 'base',
-        combine: true,
-        visible: false,
-        layers: [
-        new ol.layer.Tile({
-          source: new ol.source.Stamen({
-            layer: 'watercolor'
-          })
-        }),
-        new ol.layer.Tile({
-          source: new ol.source.Stamen({
-            layer: 'terrain-labels'
-          })
-        })
+        layers: [kokudo, osm,
+              // assets/layers/base.jsで定義したものをカンマで区切って書く
+
+              // watercolorlabels,
+              // watercolor,
         ]
       }),
-      new ol.layer.Tile({
-        title: 'Water color',
-        type: 'base',
-        visible: false,
-        source: new ol.source.Stamen({
-          layer: 'watercolor'
-        })
-      }),
-      new ol.layer.Tile({
-        title: 'OSM',
-        type: 'base',
-        visible: true,
-        source: new ol.source.OSM()
+      new ol.layer.Group({
+        title: 'Overlays',
+        layers: [aed, kml, geocycle]
       })
-      ]
-    }),
-    new ol.layer.Group({
-      title: 'Overlays',
-      layers: [vectorLayer,kml,
-        new ol.layer.Vector({
-          map: map,
-          source: new ol.source.Vector({
-            features: [accuracyFeature, positionFeature]
-          })
-          }),
-        new ol.layer.Image({
-          title: 'Countries',
-          source: new ol.source.ImageArcGISRest({
-            ratio: 1,
-            params: {'LAYERS': 'show:0'},
-            url: "https://ons-inspire.esriuk.com/arcgis/rest/services/Administrative_Boundaries/Countries_December_2016_Boundaries/MapServer"
-          })
-        })
-      ]
-    })
     ],
     view: view
   });
--- assets/layers/overlay.js
+++ assets/layers/overlay.js
@@ -1,13 +1,22 @@
-var vectorLayer = new ol.layer.Vector({
-  title: 'circle',
+// 現在地の円
+var geocycle = new ol.layer.Vector({
+                source: new ol.source.Vector({
+                  features: [accuracyFeature, positionFeature]
+                })
+              });
+
+//呉市オープンデータのaed
+//作成日:20171218
+var aed = new ol.layer.Vector({
+  title: 'AED',
   source: new ol.source.Vector({
     format: new ol.format.GeoJSON(),
-    url: 'C:/Users/kazu/Documents/GitHub/kure_kosen_map/assets/geojson/geojson.js',
+    url: 'C:/Users/kazu/Documents/GitHub/kure_kosen_map/assets/geojson/20171218.geojson',
     crossOrigin: 'anonymous'
   }),
   style: new ol.style.Style({
     image: new ol.style.Circle( /** @type {olx.style.IconOptions} */ ({
-      radius: 20,
+      radius: 10,
       fill: new ol.style.Fill({
         color: '#ffff00'
       })
@@ -15,6 +24,7 @@ var vectorLayer = new ol.layer.Vector({
   })
 });
 
+//kmlのサンプル
 var kml = new ol.layer.Vector({
   title: 'kml',
   source: new ol.source.Vector({
