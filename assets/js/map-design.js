8991d00e8ec2d75669109a58f6004b8ac3c916d6
aidmiya <a.64.83.9.4837.25234.2.36.2.65@gmail.com>
2017-12-21 00:16:03 +0900

    divでメニューの型を書き、とりあえずボタンを押せば表示されるようにした

--- assets/js/map-design.js
+++ assets/js/map-design.js
@@ -1,15 +1,21 @@
 // ボタン、メニューなどのデザイン aidmiya
-// main.jsには手を付けていません
 
-// ハンバーガーメニューのボタン。OpenLayersと「buttoncontrol.js」を使った。
+// 要素
+var menu_element = document.getElementById("menu");
+var menubtn_element = document.getElementsByClassName("menubtn");
+
+// ハンバーガーメニューのボタン。OpenLayersの「ol3-ext」(buttoncontrol.js)を使った。
+// 注意:mapが宣言されるmain.jsを読み込んでからここを読ませないと動かない
 var menubtn = new ol.control.Button ( 
- {
-  html: '<i class="fa fa-bars"></i>',
-  className: "menubtn",
-  title: "メニューを表示",
-  handleClick: function() {
-  alert("とりあえずボタンの形だけできたから、メニューを表示するときにjQuery UIを使えるかと考えているところ。\nいや先にHTMLを書かねば。\nところでフルスクリーンボタンはここでよいのか…");
- }
-});
+	{
+		html: '<i class="fa fa-bars"></i>',
+		className: "menubtn",
+		title: "メニューを表示",
+		// 注意:handleClickには、外部の関数を指定すると、その関数がページを読み込んだときに自動実行される
+		handleClick: function() {
+			menu_element.classList.toggle("active");
+		}
+	}
+);
 map.addControl(menubtn);
 
