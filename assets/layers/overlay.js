4ae1952e67359bb3abce1648b595dd232c56775f
aidmiya <a.64.83.9.4837.25234.2.36.2.65@gmail.com>
2017-12-29 12:44:49 +0900

    [fix] overlays.jsのパス

--- assets/layers/overlay.js
+++ assets/layers/overlay.js
@@ -11,12 +11,12 @@ var aed = new ol.layer.Vector({
             title: 'AED',
             source: new ol.source.Vector({
               format: new ol.format.GeoJSON(),
-              url: 'C:/Users/kazu/Documents/GitHub/kure_kosen_map/assets/geojson/20171224.geojson',
+              url: 'https://kure-kosen.github.io/kure_kosen_map/assets/geojson/20171224.geojson',
               crossOrigin: 'anonymous'
             }),
             style: new ol.style.Style({
               image: new ol.style.Icon({
-                src:"C:/Users/kazu/Documents/GitHub/kure_kosen_map/assets/icon/aed.png", scale: 0.2
+                src:"https://kure-kosen.github.io/kure_kosen_map/assets/icon/aed.png", scale: 0.2
               })
             })
           });
@@ -26,7 +26,7 @@ var kml = new ol.layer.Vector({
             title: 'kml',
             source: new ol.source.Vector({
               format: new ol.format.KML(),
-              url: 'C:/Users/kazu/Documents/GitHub/kure_kosen_map/assets/kml/kokudo.kml',
+              url: 'https://kure-kosen.github.io/kure_kosen_map/assets/kml/kokudo.kml',
               crossOrigin: 'anonymous'
             })
           });
