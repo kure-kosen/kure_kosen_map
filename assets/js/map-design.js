126461b711e2d3d0e47b8e75b97172af6bc8940c
aidmiya <31529568+aidmiya@users.noreply.github.com>
2018-01-14 21:44:57 +0900

    ol3-extによるボタンをつくるときに使う予定だったコメントを削除

--- assets/js/map-design.js
+++ assets/js/map-design.js
@@ -15,20 +15,3 @@ function toggleMenu() {
 	wrapopen_element.classList.toggle("menuactive");
 }
 
-// ハンバーガーメニューのボタン。OpenLayersの「ol3-ext」(buttoncontrol.js)を使った。
-// 注意:mapが宣言されるmain.jsを読み込んでからここを読ませないと動かない
-/*
-var menubtn = new ol.control.Button ( 
-	{
-		html: '<i class="fa fa-bars"></i>',
-		className: "menubtn",
-		title: "メニューを表示",
-		// 注意:handleClickには、外部の関数を指定すると、その関数がページを読み込んだときに自動実行される
-		handleClick: function() {
-			menu_element.classList.toggle("active");
-		}
-	}
-);
-map.addControl(menubtn);
-*/
-
