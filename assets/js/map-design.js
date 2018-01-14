0e1fb9a7a88846756ae859c955bb8230c894974d
aidmiya <31529568+aidmiya@users.noreply.github.com>
2018-01-14 22:09:34 +0900

    [Safari表示確認] メニューが開いているときに検索ボックスの幅を短くしてみた

--- assets/js/map-design.js
+++ assets/js/map-design.js
@@ -5,6 +5,7 @@ var menu_element = document.getElementById("menu");
 var openmenu_element = document.getElementById("openmenu");
 var closemenu_element = document.getElementById("closemenu");
 var wrapopen_element = document.getElementById("wrap-openmenu");
+var searchform_element = document.getElementById("search");
 
 // ハンバーガーボタン(buttonたぐ)の動作
 openmenu_element.onclick = function() {toggleMenu()};
@@ -13,5 +14,6 @@ closemenu_element.onclick = function() {toggleMenu()};
 function toggleMenu() {
 	menu_element.classList.toggle("active");
 	wrapopen_element.classList.toggle("menuactive");
+	searchform_element.classList.toggle("menuactive");
 }
 
