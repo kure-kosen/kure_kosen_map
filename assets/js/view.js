c5290c9d5bcdfe14e6b3c7cccfc66306158a74bb
kazu <kobakazu10@gmail.com>
2018-01-16 23:09:44 +0900

    初期設定を変えた

--- assets/js/view.js
+++ assets/js/view.js
@@ -1,7 +1,8 @@
 var view = new ol.View({
   center: ol.proj.fromLonLat([132.601271,34.232102]),
-  zoom: 17,
+  zoom: 17.5,
   // minZoom: 15,//挙動がおかしいからやめたほうがいいかも
   // maxZoom: 17
   // そもそもいらないかな？
+  rotation: -0.36911708366090856
 });
