fcaba94cdac71406eb2669ca00e251f639f5d6e8
aidmiya <31529568+aidmiya@users.noreply.github.com>
2018-01-21 00:30:11 +0900

    位置は悪いが[x]ボタンの形・動作(JS)を作成

--- assets/js/map-design.js
+++ assets/js/map-design.js
@@ -7,6 +7,13 @@ var closemenu_element = document.getElementById("closemenu");
 var wrapopen_element = document.getElementById("wrap-openmenu");
 var searchform_element = document.getElementById("search");
 
+// 検索ボックスの[x]ボタンの追加・動作
+var searchreset_element = document.createElement("div");
+searchreset_element.innerHTML = '<input type="reset" value="&#61453;" class="fa">';
+var search_element = document.getElementsByClassName("ol-search")[0];
+search_element.appendChild(searchreset_element);
+searchreset_element.onclick = function() {document.querySelector(".ol-search .search").value=""};
+
 // ハンバーガーボタン(buttonたぐ)の動作
 openmenu_element.onclick = function() {toggleMenu()};
 closemenu_element.onclick = function() {toggleMenu()};
