fbfe2f0c2bb565df40e295385eab30cecb6e72be
aidmiya <31529568+aidmiya@users.noreply.github.com>
2018-01-21 16:30:48 +0900

    検索ボックスの横幅を矢印ボタンが見えるように変更、placeholderを消す

--- assets/js/map-design.js
+++ assets/js/map-design.js
@@ -7,12 +7,14 @@ var closemenu_element = document.getElementById("closemenu");
 var wrapopen_element = document.getElementById("wrap-openmenu");
 var searchform_element = document.getElementById("search");
 
-// 検索ボックスの[x]ボタンの追加・動作
+// 検索ボックスの[x]ボタンの追加・動作・placeholderを消す
 var searchreset_element = document.createElement("div");
 searchreset_element.innerHTML = '<input type="reset" value="&#61453;" class="fa">';
 var search_element = document.getElementsByClassName("ol-search")[0];
 search_element.appendChild(searchreset_element);
 searchreset_element.onclick = function() {document.querySelector(".ol-search .search").value=""};
+var searchbox_element = document.querySelector('.ol-search input[type="search"]');
+searchbox_element.placeholder = "";
 
 // ハンバーガーボタン(buttonたぐ)の動作
 openmenu_element.onclick = function() {toggleMenu()};
