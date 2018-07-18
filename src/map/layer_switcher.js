a0da673b87bc23a43189b498ac1fd0eed87f9a05
kobakakzu0429 <kobakazu10@gmail.com>
2018-07-18 23:30:48 +0900

    refactoring

--- src/map/layer_switcher.js
+++ src/map/layer_switcher.js
@@ -1,101 +1,129 @@
 import Sortable from 'sortablejs'
+
 import './css/layer_switcher.css'
 
-const layer_switcher_wrapper = $('<div>', {
-  id: 'layer_switcher_wrapper'
-}).appendTo($('.fullscreenlayer'))
+// Creating Elements
+const $layer_switcher_wrapper = $('<div>')
+  .attr('id', 'layer_switcher_wrapper')
+  .appendTo($('.fullscreenlayer'))
 
-const table = $('<table>', {
-  id: 'layer_switcher'
-}).appendTo(layer_switcher_wrapper)
+const $table = $('<table>')
+  .attr('id', 'layer_switcher')
+  .appendTo($layer_switcher_wrapper)
+// ------------------------------------------------
 
+// Function
 const bindSlider = (slider, layer) => {
   slider.on('input change', e => {
     layer.setOpacity(parseFloat(e.target.value))
   })
   slider.val(String(layer.getOpacity()))
 }
+// end bindSlider ---------------------------------
 
-const bindCheckbox = (visible, layer) => {
-  visible.on('change', e => {
+const bindCheckbox = (checkbox, layer) => {
+  checkbox.on('change', e => {
     layer.setVisible(e.target.checked)
   })
-  visible.prop('checked', layer.getVisible())
+  checkbox.prop('checked', layer.getVisible())
 }
+// end bindCheckbox -------------------------------
 
-const MyLayerSwitcher = map => {
-  const layers = map.getLayers().getArray()
-  layers.forEach(layer_group => {
-    layer_group.values_.layers.getArray().forEach((layer, i) => {
-      if (layer_group.values_.title == 'Base maps') return // とりあえず
-      let row = $('<tr>', {
-        id: 'layer' + i
-      })
+const createTr = (layer_group, layer, i) => {
+  const $row = $('<tr>').attr('id', 'layer' + i)
 
-      let td1 = $('<td>', {
-        class: 'movable'
-      }).text(layer_group.values_.title)
-      let td2 = $('<td>', {
-        class: 'movable'
-      }).text(layer.values_.title)
-      let input = $('<input>', {
-        class: 'opacity',
-        type: 'range',
-        min: '0',
-        max: '1',
-        step: '0.01'
-      })
-      let td3 = $('<td>').append(input)
-      let visible = $('<input>', {
-        class: 'visible',
-        type: 'checkbox'
-      })
-      let td4 = $('<td>').append(visible)
-      bindSlider(input, layer)
-      bindCheckbox(visible, layer)
-      row.append(td1)
-      row.append(td2)
-      row.append(td3)
-      row.append(td4)
-      table.append(row)
-    })
+  const $td1 = $('<td>')
+    .attr('class', 'movable')
+    .text(layer_group.values_.title)
+
+  const $td2 = $('<td>')
+    .attr('class', 'movable')
+    .text(layer.values_.title)
+
+  const $slider = $('<input>', {
+    class: 'opacity',
+    type: 'range',
+    min: '0',
+    max: '1',
+    step: '0.01'
   })
-  table.appendTo(layer_switcher)
 
-  Sortable.create(layer_switcher, {
+  const $td3 = $('<td>').append($slider)
+
+  const $visible_checkbox = $('<input>', {
+    class: 'visible',
+    type: 'checkbox'
+  })
+
+  const $td4 = $('<td>').append($visible_checkbox)
+
+  $row.append($td1)
+  $row.append($td2)
+  $row.append($td3)
+  $row.append($td4)
+
+  $table.append($row)
+
+  bindSlider($slider, layer)
+  bindCheckbox($visible_checkbox, layer)
+}
+// end createTr -----------------------------------
+// ------------------------------------------------
+
+// Sort
+const SortInit = layers => {
+  Sortable.create($('#layer_switcher').get(0), {
     handle: '.movable',
     animation: 100,
     onUpdate: () => {
-      getOrderInBase(layers)
+      setZIndexOrderByDesc(layers)
     }
   })
+  // end Sortable.create --------------------------
 
-  const getOrderInBase = layers => {
-    const array = table
+  const setZIndexOrderByDesc = layers => {
+    const layerJoinZIndex = {}
+
+    const arrayDesc = $table
       .children()
       .toArray()
       .reverse()
 
-    let tableObj = {}
-    array.forEach((v, i) => {
-      let hoge = $(v)
+    arrayDesc.forEach((v, i) => {
+      let $layer = $(v)
         .children()
         .eq(1)
         .html()
-      tableObj[hoge] = i
+      layerJoinZIndex[$layer] = i
     })
 
     layers.forEach(layer_group => {
       layer_group.values_.layers.getArray().forEach(layer => {
-        let title = layer.values_.title
-        Object.keys(tableObj).forEach(function(key, value) {
-          if (title === key) {
+        Object.keys(layerJoinZIndex).forEach((key, value) => {
+          if (layer.values_.title === key) {
             layer.setZIndex(value)
           }
         })
       })
     })
   }
+  // end setZIndexOrderByDesc----------------------
+}
+// ------------------------------------------------
+
+// Main
+const MyLayerSwitcher = map => {
+  const layers = map.getLayers().getArray()
+  layers.forEach(layer_group => {
+    layer_group.values_.layers.getArray().forEach((layer, i) => {
+      if (layer_group.values_.title == 'Base maps') return // とりあえず
+      createTr(layer_group, layer, i)
+    })
+  })
+
+  $table.appendTo($layer_switcher_wrapper)
+
+  SortInit(layers)
 }
 
 export default MyLayerSwitcher
