cb4f8b7776e7884d36775cd7568865ac8ee9fbf6
kobakakzu0429 <kobakazu10@gmail.com>
2018-07-22 14:22:01 +0900

    separate create layer function

--- src/map/layers/fetch.js
+++ src/map/layers/fetch.js
@@ -8,29 +8,32 @@ import axios from 'axios'
 
 import 'babel-polyfill'
 
+const createLayer = data => {
+  return new TileLayer({
+    id: data.id,
+    type: 'base',
+    group: data.group,
+    category: data.category,
+    name: data.name,
+    visible: false,
+    source: new XYZSource({
+      url: data.url,
+      attributions: [
+        new Attribution({
+          html: data.attributions
+        })
+      ]
+    })
+  })
+}
+
 const fetchAddlayer = map => {
   axios
     .get('http://localhost:3000/layer')
     .then(response => {
       const result = response.data
       for (let i = 0; i < 4; i++) {
-        let tmp = new TileLayer({
-          id: i + 6,
-          type: 'base',
-          group: result[i]['group'],
-          category: result[i]['category'],
-          name: result[i]['name'],
-          visible: false,
-          source: new XYZSource({
-            url: result[i]['url'],
-            attributions: [
-              new Attribution({
-                html: result[i]['attributions']
-              })
-            ]
-          })
-        })
-        map.addLayer(tmp)
+        map.addLayer(createLayer(result[i]))
       }
       MyLayerSwitcher(map)
     })
