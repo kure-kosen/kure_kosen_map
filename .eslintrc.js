7b553081e3dfeb0bf4961ae712cb5dba702f96e8
kobakakzu0429 <kobakazu10@gmail.com>
2018-08-08 00:29:59 +0900

    to parserOptions.ecmaVersion: 8

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   extends: ['eslint:recommended'],
   parserOptions: {
     parser: 'babel-eslint',
-    ecmaVersion: 6,
+    ecmaVersion: 8,
     sourceType: 'module'
   },
   rules: {
