57d2d430e1bb0c0b21104c1446941c85b5b5223f
aidmiya <31529568+aidmiya@users.noreply.github.com>
2018-12-26 02:30:20 +0900

    ダミー等項目の削除とリスト内容の見直し

--- kure_kosen_map/pages/map.vue
+++ kure_kosen_map/pages/map.vue
@@ -11,44 +11,23 @@
             <div>建物ジャンプ</div>
             <div class="menu-buildings">
               <ul>
-                <li>図書館棟<br>(教育センター)</li>
+                <li>図書館棟 (教育センター)</li>
                 <li>管理棟</li>
                 <li>普通教室棟</li>
                 <li>機械工学科棟</li>
                 <li>電気情報工学科棟</li>
                 <li>建築学科棟</li>
                 <li>環境都市工学科棟</li>
-                <li>専攻科棟<br> (協働研究センター棟)</li>
+                <li>専攻科棟 (協働研究センター棟)</li>
                 <li>第2普通教室棟</li>
                 <li>静心館</li>
-                <li>(第1)体育館</li>
+                <li>第1体育館</li>
                 <li>第2体育館</li>
-                <li>(学生)食堂</li>
+                <li>学生食堂</li>
                 <li>技術センター</li>
-                <li>ダミー</li>
-                <li>ダミー</li>
-                <li>ダミー</li>
-                <li>ダミー</li>
-                <li>ダミー</li>
-                <li>ダミー</li>
               </ul>
             </div>
             <div>
-              <a
-                href="#manual"
-                title="コンテンツ準備中">
-                <div>準備中</div>
-              </a>
-              <a
-                href="#history"
-                title="コンテンツ準備中">
-                <div>準備中</div>
-              </a>
-              <a
-                href="#terms"
-                title="コンテンツ準備中">
-                <div>準備中</div>
-              </a>
               <a
                 href="about_us.html"
                 title="About Us">
