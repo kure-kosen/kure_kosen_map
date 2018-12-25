cff9c8c22944156552d1a2f87a52c9113d8d03e1
aidmiya <31529568+aidmiya@users.noreply.github.com>
2018-12-26 02:06:52 +0900

    SPはslideMenuでtoggle可能になった

--- kure_kosen_map/pages/map.vue
+++ kure_kosen_map/pages/map.vue
@@ -2,71 +2,75 @@
   <div>
     <MyMap />
     <div class="fullscreenlayer">
-      <nav
-        id="menu"
-        class="menu">
-        <div class="menu-box">
-          <div class="menu-title">呉高専マップ</div>
-          <div>建物ジャンプ</div>
+      <slideMenu>
 
-          <div class="menu-buildings">
-            <ul>
-              <li>図書館棟<br>(教育センター)</li>
-              <li>管理棟</li>
-              <li>普通教室棟</li>
-              <li>機械工学科棟</li>
-              <li>電気情報工学科棟</li>
-              <li>建築学科棟</li>
-              <li>環境都市工学科棟</li>
-              <li>専攻科棟<br> (協働研究センター棟)</li>
-              <li>第2普通教室棟</li>
-              <li>静心館</li>
-              <li>(第1)体育館</li>
-              <li>第2体育館</li>
-              <li>(学生)食堂</li>
-              <li>技術センター</li>
-              <li>ダミー</li>
-              <li>ダミー</li>
-              <li>ダミー</li>
-              <li>ダミー</li>
-              <li>ダミー</li>
-              <li>ダミー</li>
-            </ul>
+        <nav
+          id="menu"
+          class="menu">
+          <div class="menu-box">
+            <div class="menu-title">呉高専マップ</div>
+            <div>建物ジャンプ</div>
+
+            <div class="menu-buildings">
+              <ul>
+                <li>図書館棟<br>(教育センター)</li>
+                <li>管理棟</li>
+                <li>普通教室棟</li>
+                <li>機械工学科棟</li>
+                <li>電気情報工学科棟</li>
+                <li>建築学科棟</li>
+                <li>環境都市工学科棟</li>
+                <li>専攻科棟<br> (協働研究センター棟)</li>
+                <li>第2普通教室棟</li>
+                <li>静心館</li>
+                <li>(第1)体育館</li>
+                <li>第2体育館</li>
+                <li>(学生)食堂</li>
+                <li>技術センター</li>
+                <li>ダミー</li>
+                <li>ダミー</li>
+                <li>ダミー</li>
+                <li>ダミー</li>
+                <li>ダミー</li>
+                <li>ダミー</li>
+              </ul>
+            </div>
+            <div>
+              <a
+                href="#manual"
+                title="コンテンツ準備中">
+                <div>準備中</div>
+              </a>
+              <a
+                href="#history"
+                title="コンテンツ準備中">
+                <div>準備中</div>
+              </a>
+            </div>
+            <div>
+              <a
+                href="#terms"
+                title="コンテンツ準備中">
+                <div>準備中</div>
+              </a>
+              <a
+                href="about_us.html"
+                title="About Us">
+                <div>About Us</div>
+              </a>
+              <a
+                href="https://www.kure-nct.ac.jp/"
+                title="呉高専HPへ">
+                <div>呉高専HPへ</div>
+              </a>
+            </div>
+            <div class="copyright">
+              &copy;2018 - Kuremap Team
+            </div>
           </div>
-          <div>
-            <a
-              href="#manual"
-              title="コンテンツ準備中">
-              <div>準備中</div>
-            </a>
-            <a
-              href="#history"
-              title="コンテンツ準備中">
-              <div>準備中</div>
-            </a>
-          </div>
-          <div>
-            <a
-              href="#terms"
-              title="コンテンツ準備中">
-              <div>準備中</div>
-            </a>
-            <a
-              href="about_us.html"
-              title="About Us">
-              <div>About Us</div>
-            </a>
-            <a
-              href="https://www.kure-nct.ac.jp/"
-              title="呉高専HPへ">
-              <div>呉高専HPへ</div>
-            </a>
-          </div>
-          <div class="copyright">
-            &copy;2018 - Kuremap Team
-          </div>
-        </div>
-      </nav>
+        </nav>
+
+      </slideMenu>
 
     </div>
   </div>
@@ -74,10 +78,12 @@
 
 <script>
 import MyMap from "../components/mymap";
+import slideMenu from "../components/slideMenuFromTop";
 
 export default {
   components: {
-    MyMap
+    MyMap,
+    slideMenu
   }
 };
 </script>
