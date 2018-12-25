13f6588b0255e0620f872f9db04f501680ec7f36
kobakakzu0429 <kobakazu10@gmail.com>
2018-12-25 21:34:55 +0900

    create map function

---
+++ kure_kosen_map/assets/map/initMap.js
@@ -0,0 +1,35 @@
+import "ol/ol.css";
+
+import Map from "ol/Map";
+import View from "ol/View";
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
+import { fromLonLat } from "ol/proj";
+import { ScaleLine } from "ol/control";
+import { defaults as defaultInteractions, DragRotateAndZoom } from "ol/interaction";
+
+export const initMap = vm => {
+  const view = new View({
+    center: fromLonLat([140.097, 37.856]),
+    zoom: 8
+  });
+
+  const layers = [
+    new TileLayer({
+      source: new OSM()
+    })
+  ];
+
+  const interactions = defaultInteractions().extend([new DragRotateAndZoom()]);
+
+  const map = new Map({
+    target: "mymap",
+    interactions,
+    layers,
+    view
+  });
+
+  map.addControl(new ScaleLine());
+
+  vm.$store.commit("setMap", map);
+};
