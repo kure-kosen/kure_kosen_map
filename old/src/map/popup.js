224e12c7d5140a7a8b87a2f78b0bc8ed7fa39a41
kobakakzu0429 <kobakazu10@gmail.com>
2018-12-26 11:32:41 +0900

    remove old/popup and old/select

--- old/src/map/popup.js
+++
@@ -1,9 +0,0 @@
-import ol_Overlay_Popup from 'ol-ext/overlay/Popup'
-import 'ol-ext/overlay/Popup.css'
-
-const MyPopup = new ol_Overlay_Popup({
-  closeBox: true,
-  positioning: 'auto'
-})
-
-export default MyPopup
