e84c872bf45f5ce594a4cb9e0aaea75ccad36bae
kobakakzu0429 <kobakazu10@gmail.com>
2019-01-03 23:34:08 +0900

    good

--- kure_kosen_map/components/map/layerSwitcherWrapper.vue
+++ kure_kosen_map/components/map/layerSwitcherWrapper.vue
@@ -1,47 +1,67 @@
 <template>
   <div>
     <div
-      id="layer_switcher_button_wrapper"
-      class="ol-unselectable ol-control"
+      class="ol-unselectable ol-control layer_switcher_button_wrapper"
     >
-      <button @click="openLayerSwitcher">
+      <button @click="open">
         <i class="far fa-clone" />
       </button>
     </div>
 
-    <VueDragResize
+    <div
+      v-draggable="draggableOptions"
       v-show="isOpen"
-      :w="500"
-      :h="500"
-      :x="100"
-      :y="100"
-    >
-      <layerSwitcher @close="closeLayerSwitcher" />
-    </VueDragResize>
+      class="window">
+      <div
+        ref="handle"
+        class="header">
+        <div class="close_button_wrapper">
+          <button @click="close">
+            <i class="fas fa-times" />
+          </button>
+        </div>
+      </div>
+
+      <layerLists />
+      <layerSwitcher />
+
+    </div>
   </div>
 </template>
 
 <script>
-import VueDragResize from "vue-drag-resize";
+import { Draggable } from "draggable-vue-directive";
 import layerSwitcher from "./layerSwitcher";
+import layerLists from "./layerLists";
 
 export default {
   components: {
-    VueDragResize,
-    layerSwitcher
+    layerSwitcher,
+    layerLists
+  },
+
+  directives: {
+    Draggable
   },
 
   data() {
     return {
-      isOpen: false
+      isOpen: false,
+      draggableOptions: {
+        handle: undefined,
+        initialPosition: { left: 100, top: 100 }
+      }
     };
+  },
+  mounted() {
+    this.draggableOptions.handle = this.$refs.handle;
   },
 
   methods: {
-    openLayerSwitcher() {
+    open() {
       this.isOpen = true;
     },
-    closeLayerSwitcher() {
+    close() {
       this.isOpen = false;
     }
   }
@@ -49,9 +69,39 @@ export default {
 </script>
 
 <style scoped>
-#layer_switcher_button_wrapper {
+.layer_switcher_button_wrapper {
   position: absolute;
   top: 290px;
   left: 0.5em;
+}
+
+.window {
+  position: absolute;
+  width: 500px;
+  height: 500px;
+  border: solid 5px #0da3e3;
+  border-radius: 3px;
+  background-color: #fff;
+}
+
+.header {
+  height: 18px;
+  background-color: #0da3e3;
+  cursor: pointer;
+}
+
+.header i {
+  color: #fff;
+  font-size: 18px;
+  line-height: 18px;
+}
+
+.close_button_wrapper {
+  float: right;
+  vertical-align: middle;
+  font-size: 16px;
+  margin-top: -1px;
+  margin-right: 1px;
+  text-align: right;
 }
 </style>
